1be841476a2ec9feef2a8e09cc43e5d7b135a37a
cr13 <40727096+sejoule-ju5t@users.noreply.github.com>
2022-01-13 17:25:21 +0900

    Update crawler_selenium.py

--- Crawler/crawler_selenium.py
+++ Crawler/crawler_selenium.py
@@ -8,3 +8,17 @@ from lib2to3.pgen2 import driver
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('www.baidu.com')
+
+## 获取元素
+driver.find_element_by_id("id")
+driver.find_element_by_name("name")
+
+## xpath路径的方式来获取元素
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com/')
+search_input = driver.find_element_by_id("kw") # 获取到百度搜索框
+search_input.send_keys("刘亦菲")  # 自动输入 刘亦菲
+submit = driver.find_element_by_id("su")  # 获取到百度一下按钮
+submit.click()  # 点击搜索
+
